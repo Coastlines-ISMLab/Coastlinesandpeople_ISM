d6ddebd2b7bdc74439283f288f7b07a97fe72627
Alisha Dhawan <alisha.dhawan17@gmail.com>
2018-09-19 14:54:02 -0400

    error

--- wear/src/main/java/com/example/alish/coastlinesandpeople_ism/MainActivity.java
+++ wear/src/main/java/com/example/alish/coastlinesandpeople_ism/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends WearableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextView = (TextView) findViewById(R.id.text);
 
         // Enables Always-on

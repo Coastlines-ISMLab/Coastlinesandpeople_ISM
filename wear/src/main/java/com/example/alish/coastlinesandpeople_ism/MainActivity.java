9b2c988fa80d21acb6a09ac0a33ca6db42b69665
Vidit Virmani <30908674+viditvir@users.noreply.github.com>
2018-10-01 17:43:16 -0400

    Added a dialog box implementation in MainActivity for Wear app just to test. The implementation can be used in the android version if there is a requirement. Changes committed are not permanent, feel free to update files if need be.

--- wear/src/main/java/com/example/alish/coastlinesandpeople_ism/MainActivity.java
+++ wear/src/main/java/com/example/alish/coastlinesandpeople_ism/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.alish.coastlinesandpeople_ism;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.text);
+        Button btn = findViewById(R.id.your_button_id);
+        btn.setText("Click to create dialog");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Close the dialog");
+                builder.setPositiveButton("OK", null);
+                builder.setNegativeButton("Cancel", null);
+                builder.show();
 
+            }
+        });
         // Enables Always-on
         setAmbientEnabled();
     }
